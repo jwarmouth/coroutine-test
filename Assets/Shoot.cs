dae704ffacc69ac39b9e66c0d8d8cad5ab9f45e2
Jeff Warmouth <jwarmouth@fitchburgstate.edu>
2021-10-12 11:46:57 -0400

    Add Unity Project

---
+++ Assets/Shoot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shotSpawn;
+    public Transform bulletContainer;
+    public float shotCooldown;
+    public bool canShoot = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canShoot)
+        {
+            StartCoroutine("ShootWithCooldown");
+        }
+    }
+
+    IEnumerator ShootWithCooldown ()
+    {
+        // stuff before the delay
+        Instantiate(bulletPrefab, shotSpawn.position, shotSpawn.rotation, bulletContainer);
+        canShoot = false;
+        Debug.Log(bulletContainer.childCount + " Bullets Shot");
+
+        // the delay
+        yield return new WaitForSeconds(shotCooldown);
+
+        // stuff after the delay
+        canShoot = true;
+
+    }
+
+
+}
